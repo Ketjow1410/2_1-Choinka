0f77c30e52c47d1746d255c449851923ab8f7725
ketjow <100692239+Ketjow1410@users.noreply.github.com>
2022-03-29 10:59:09 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+    Scanner wpisz = new Scanner(System.in);
+    int w;
+System.out.println();
+    System.out.print(" podaj wysokosc");
+    w=wpisz.nextInt();
+  for(int i=0; i<=w;i++){
+        System.out.println(" ");
+      for(int j=0;j<=i;j++)
+        System.out.print("*");
+    }
+   
+      }}
+  
